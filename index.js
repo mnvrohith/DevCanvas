d8a2c6eec177c57c8d0bfce1fefdbaa07a0b2f9a
mnvrohith <mnvrohith@gmail.com>
2024-09-01 22:59:32 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,28 @@
+document.getElementById("html-play").addEventListener("click", function(){
+    var htmlCode = document.getElementById("html-code").value;
+    var output = document.getElementById("output");
+    output.contentDocument.body.innerHTML= htmlCode;
+   
+});
+document.getElementById("css-play").addEventListener("click",function (){
+    let htmlCode = document.getElementById("html-code").value;
+    let cssCode = document.getElementById("css-code").value;
+    var output = document.getElementById("output");
+    output.contentDocument.body.innerHTML= htmlCode +"<style>"+cssCode+"</style>";
+});
+document.getElementById("js-play").addEventListener("click",function (){
+    var htmlCode = document.getElementById("html-code").value;
+    var cssCode = document.getElementById("css-code").value;
+    var jsCode = document.getElementById("js-code").value;
+    var output = document.getElementById("output");
+    output.contentDocument.body.innerHTML= htmlCode+"<style>" +cssCode +"</style>";
+    output.contentWindow.eval(jsCode);
+});
+document.getElementById("output-play").addEventListener("click",function (){
+    let htmlCode = document.getElementById("html-code").value;
+    let cssCode = document.getElementById("css-code").value;
+    let jsCode = document.getElementById("js-code").value;
+    let output = document.getElementById("output");
+    output.contentDocument.body.innerHTML= htmlCode+"<style>" +cssCode +"</style>";
+    output.contentWindow.eval(jsCode);
+});
