2b794a64215e19443efa16b72304c5c90f4a3b7f
MNV Rohith <mnvrohith@gmail.com>
2025-03-03 22:51:47 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,17 +5,17 @@ document.getElementById("html-play").addEventListener("click", function(){
    
 });
 document.getElementById("css-play").addEventListener("click",function (){
-    let htmlCode = document.getElementById("html-code").value;
+    // let htmlCode = document.getElementById("html-code").value;
     let cssCode = document.getElementById("css-code").value;
     var output = document.getElementById("output");
-    output.contentDocument.body.innerHTML= htmlCode +"<style>"+cssCode+"</style>";
+    output.contentDocument.body.innerHTML="<style>"+cssCode+"</style>";
 });
 document.getElementById("js-play").addEventListener("click",function (){
-    var htmlCode = document.getElementById("html-code").value;
-    var cssCode = document.getElementById("css-code").value;
+    // var htmlCode = document.getElementById("html-code").value;
+    // var cssCode = document.getElementById("css-code").value;
     var jsCode = document.getElementById("js-code").value;
     var output = document.getElementById("output");
-    output.contentDocument.body.innerHTML= htmlCode+"<style>" +cssCode +"</style>";
+    // output.contentDocument.body.innerHTML= htmlCode+"<style>" +cssCode +"</style>";
     output.contentWindow.eval(jsCode);
 });
 document.getElementById("output-play").addEventListener("click",function (){
